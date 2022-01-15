ae7ec34f8f86a809bed53c7990e9db26d6d1912a
Daniel Franklin <daniel@danielzfranklin.org>
2022-01-15 11:28:06 +0000

    Non-working attempt at using builtin smart led

--- blong/src/main.rs
+++ blong/src/main.rs
@@ -16,17 +16,25 @@ mod app {
     #[allow(unused_imports)]
     use defmt::{debug, error, info, warn, Format};
 
+    use apa102_spi::Apa102;
+    use hal::gpio::Level;
+    use hal::spi::{self, Spi};
     use nrf52840_hal as hal;
+    use smart_leds_trait::{SmartLedsWrite, RGB8};
 
     use hal::gpiote::Gpiote;
-    use hal::pac::TIMER0;
+    use hal::pac::{spi0, SPI0, TIMER0};
 
     // A monotonic timer to enable scheduling in RTIC
     #[monotonic(binds = TIMER0, default = true)]
     type MonoDefault = MonoTimer<TIMER0>;
 
+    type DotStar = Apa102<Spi<SPI0>>;
+
     #[shared]
-    struct Shared {}
+    struct Shared {
+        dotstar: DotStar,
+    }
 
     #[local]
     struct Local {
@@ -43,10 +51,29 @@ mod app {
         //   See https://developer.arm.com/docs/100737/0100/power-management/sleep-mode/sleep-on-exit-bit
         cx.core.SCB.set_sleepdeep();
 
-        let port0 = hal::gpio::p0::Parts::new(cx.device.P0);
         let gpiote = Gpiote::new(cx.device.GPIOTE);
+        let port0 = hal::gpio::p0::Parts::new(cx.device.P0);
+        let port1 = hal::gpio::p1::Parts::new(cx.device.P1);
 
-        // Setup button
+        // Setup builtin dotstar led
+        let dotstar_pins = spi::Pins {
+            // Pin numbers from <https://blog.adafruit.com/2021/05/26/pin-reference-adafruit-itsybitsy-nrf52840-prettypins/>
+            sck: port1.p1_09.into_push_pull_output(Level::High).degrade(),
+            mosi: Some(port0.p0_08.into_push_pull_output(Level::High).degrade()),
+            miso: None,
+        };
+
+        let dotstar_spi = Spi::new(
+            cx.device.SPI0,
+            dotstar_pins,
+            spi0::frequency::FREQUENCY_A::K125,
+            apa102_spi::MODE,
+        );
+        // Not inverting end frame b/c adafruit does something similar in their bsp for other boards
+        // See <https://github.com/atsamd-rs/atsamd/blob/9495af975d6a35ae8bb455fae29ad0356fe20e09/boards/trinket_m0/src/lib.rs#L155>
+        let dotstar = Apa102::new_with_custom_postamble(dotstar_spi, 4, false);
+
+        // Setup builtin button
         gpiote
             .channel0()
             .input_pin(&port0.p0_29.into_pullup_input().degrade())
@@ -56,7 +83,11 @@ mod app {
         // Spawn task, runs right after init finishes
         startup::spawn().unwrap();
 
-        (Shared {}, Local { gpiote }, init::Monotonics(mono_default))
+        (
+            Shared { dotstar },
+            Local { gpiote },
+            init::Monotonics(mono_default),
+        )
     }
 
     // Background task, runs whenever no other tasks are running
@@ -72,17 +103,24 @@ mod app {
     }
 
     // Software task, also not bound to a hardware interrupt
-    #[task]
-    fn startup(_cx: startup::Context) {
+    #[task(shared = [dotstar])]
+    fn startup(mut cx: startup::Context) {
         info!("Starting up");
+        cx.shared.dotstar.lock(|dotstar| {
+            dotstar.write([RGB8::new(64, 64, 64)].into_iter()).unwrap();
+        });
     }
 
-    #[task(binds = GPIOTE, local = [gpiote])]
-    fn on_gpiote(cx: on_gpiote::Context) {
+    #[task(binds = GPIOTE, local = [gpiote], shared = [dotstar])]
+    fn on_gpiote(mut cx: on_gpiote::Context) {
         let gpiote = cx.local.gpiote;
 
         if gpiote.channel0().is_event_triggered() {
             debug!("Button press");
+
+            cx.shared.dotstar.lock(|dotstar| {
+                dotstar.write([RGB8::new(0, 0, 64)].into_iter()).unwrap();
+            });
         }
 
         gpiote.reset_events();
